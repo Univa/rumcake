13cbf73c99a33c5b4223c97a42b9c3f4faca7410
Univa <41708691+Univa@users.noreply.github.com>
2023-10-10 21:05:17 -0400

    move default display rendering back to rumcake crate

--- rumcake-drivers/src/ssd1306.rs
+++ rumcake-drivers/src/ssd1306.rs
@@ -1,12 +1,8 @@
 use core::fmt::Debug;
 
-use embedded_graphics::mono_font::ascii::FONT_6X10;
-use embedded_graphics::mono_font::{MonoTextStyle, MonoTextStyleBuilder};
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::DrawTarget;
 use embedded_hal::blocking::i2c::Write;
-use embedded_text::alignment::HorizontalAlignment;
-use embedded_text::style::{HeightMode, TextBoxStyle, TextBoxStyleBuilder};
 use heapless::String;
 use ssd1306::mode::BufferedGraphicsMode;
 use ssd1306::prelude::{DisplayConfig, I2CInterface};
@@ -14,29 +10,12 @@ use ssd1306::rotation::DisplayRotation;
 use ssd1306::size::{DisplaySize, DisplaySize128x32};
 use ssd1306::{I2CDisplayInterface, Ssd1306};
 
-use crate::on_update_default;
-
-use rumcake::display::drivers::DisplayDriver;
+use rumcake::display::drivers::{on_update_default, DisplayDriver, Orientation};
 use rumcake::display::DisplayDevice;
 
 pub mod driver {
     pub use ssd1306::*;
 }
-
-pub static DEFAULT_STYLE: MonoTextStyle<'_, BinaryColor> = MonoTextStyleBuilder::new()
-    .font(&FONT_6X10)
-    .text_color(BinaryColor::On)
-    .build();
-
-pub static DEFAULT_TEXTBOX_STYLE: TextBoxStyle = TextBoxStyleBuilder::new()
-    .height_mode(HeightMode::FitToText)
-    .alignment(HorizontalAlignment::Left)
-    .build();
-
-pub static DEFAULT_HEADER_STYLE: TextBoxStyle = TextBoxStyleBuilder::new()
-    .height_mode(HeightMode::FitToText)
-    .alignment(HorizontalAlignment::Center)
-    .build();
 
 pub trait Ssd1306I2cDisplayDriver<S: DisplaySize = DisplaySize128x32>: DisplayDevice {
     const SIZE: S;
@@ -57,10 +36,10 @@ pub trait Ssd1306I2cDisplayDriver<S: DisplaySize = DisplaySize128x32>: DisplayDe
     ) {
         match Self::ROTATION {
             DisplayRotation::Rotate0 | DisplayRotation::Rotate180 => {
-                on_update_default!(display, "HORIZONTAL", 12);
+                on_update_default(display, Orientation::Horizontal, 8).await;
             }
             DisplayRotation::Rotate90 | DisplayRotation::Rotate270 => {
-                on_update_default!(display, "VERTICAL", 8);
+                on_update_default(display, Orientation::Vertical, 12).await;
             }
         }
     }
--- rumcake/src/display/drivers.rs
+++ rumcake/src/display/drivers.rs
@@ -1,4 +1,128 @@
 use super::DisplayDevice;
+use core::fmt::Debug;
+
+use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::mono_font::{MonoTextStyle, MonoTextStyleBuilder};
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_text::alignment::HorizontalAlignment;
+use embedded_text::style::{HeightMode, TextBoxStyle, TextBoxStyleBuilder};
+use heapless::String;
+
+pub static DEFAULT_STYLE: MonoTextStyle<'_, BinaryColor> = MonoTextStyleBuilder::new()
+    .font(&FONT_6X10)
+    .text_color(BinaryColor::On)
+    .build();
+
+pub static DEFAULT_TEXTBOX_STYLE: TextBoxStyle = TextBoxStyleBuilder::new()
+    .height_mode(HeightMode::FitToText)
+    .alignment(HorizontalAlignment::Left)
+    .build();
+
+pub static DEFAULT_HEADER_STYLE: TextBoxStyle = TextBoxStyleBuilder::new()
+    .height_mode(HeightMode::FitToText)
+    .alignment(HorizontalAlignment::Center)
+    .build();
+
+macro_rules! text_box {
+    ($box:expr, "textbox", $text:expr) => {
+        embedded_text::TextBox::with_textbox_style(
+            $text,
+            $box,
+            DEFAULT_STYLE,
+            DEFAULT_TEXTBOX_STYLE,
+        )
+    };
+    ($box:expr, "text", $text:expr) => {
+        embedded_graphics::text::Text::new(
+            $text,
+            embedded_graphics::prelude::Point::zero(),
+            DEFAULT_STYLE,
+        )
+    };
+}
+
+// TODO: fix default horizontal layout overflow if "FlowLayout" ever gets implemented: https://github.com/bugadani/embedded-layout/issues/8
+macro_rules! on_update_default {
+    ($display:ident, "HORIZONTAL", $margin:expr) => {
+        on_update_default!($display, horizontal, $margin, "text")
+    };
+    ($display:ident, "VERTICAL", $margin:expr) => {
+        on_update_default!($display, vertical, $margin, "textbox")
+    };
+    ($display:ident, $direction:ident, $margin:expr, $text_type:tt) => {
+        use embedded_graphics::Drawable;
+        use embedded_layout::prelude::Align;
+
+        let bounding_box = $display.bounding_box();
+
+        // Empty chain
+        let contents = embedded_layout::prelude::Chain::new(embedded_graphics::text::Text::new(
+            "",
+            embedded_graphics::prelude::Point::zero(),
+            DEFAULT_STYLE,
+        ));
+
+        // Battery level
+        #[cfg(feature = "nrf-ble")]
+        let battery_level = {
+            let mut string: String<8> = String::from("BAT: ");
+            string
+                .push_str(&String::<3>::from(
+                    crate::hw::BATTERY_LEVEL_STATE.get().await,
+                ))
+                .unwrap();
+            string
+        };
+
+        #[cfg(feature = "nrf-ble")]
+        let contents = contents.append(text_box!(bounding_box, $text_type, &battery_level));
+
+        // Mode
+        #[cfg(all(feature = "usb", feature = "bluetooth"))]
+        let contents = contents.append(text_box!(
+            bounding_box,
+            $text_type,
+            if crate::usb::USB_STATE.get().await {
+                "MODE: USB"
+            } else {
+                "MODE: BT"
+            }
+        ));
+
+        embedded_layout::layout::linear::LinearLayout::$direction(contents)
+            .with_spacing(embedded_layout::layout::linear::FixedMargin($margin))
+            .align_to(
+                &bounding_box,
+                embedded_layout::prelude::horizontal::Left,
+                embedded_layout::prelude::vertical::Top,
+            )
+            .arrange()
+            .draw($display)
+            .unwrap();
+    };
+}
+
+use embedded_graphics::prelude::DrawTarget;
+
+pub enum Orientation {
+    Vertical,
+    Horizontal,
+}
+
+pub async fn on_update_default(
+    display: &mut impl DrawTarget<Color = BinaryColor, Error = impl Debug>,
+    orientation: Orientation,
+    margin: i32,
+) {
+    match orientation {
+        Orientation::Vertical => {
+            on_update_default!(display, "VERTICAL", margin);
+        }
+        Orientation::Horizontal => {
+            on_update_default!(display, "HORIZONTAL", margin);
+        }
+    }
+}
 
 pub trait DisplayDriver<K: DisplayDevice> {
     /// Called every time a data source updates, or every frame if DisplayDevice::FPS is non-zero.
